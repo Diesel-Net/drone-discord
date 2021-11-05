8163b97fa7a600fedfc259c2ff19b7bcdee01da3
Thomas Daley <tomdaley92@gmail.com>
2021-11-04 19:39:19 -0700

    Print drone guild members

--- bot.py
+++ bot.py
@@ -18,7 +18,10 @@ async def on_ready():
 
     print(
         f'{client.user} is connected to the following guild:\n'
-        f'{guild.name}(id: {guild.id})'
+        f'{guild.name}(id: {guild.id})\n'
     )
+
+    members = '\n - '.join([member.name for member in guild.members])
+    print(f'Guild Members:\n - {members}')
 
 client.run(TOKEN)
