969fc65c59f4c371b16750e53748dbcea07e4d56
Thomas Daley <tomdaley92@gmail.com>
2021-11-08 15:22:11 -0800

    adjust base path for test curl

--- test_curl.sh
+++ test_curl.sh
@@ -3,4 +3,4 @@ curl -XPOST \
 -H 'Date: Wed, 28 Oct 2020 23:05:22 GMT' \
 -H 'Digest: SHA-256=9H4/Wp3eNf5Ucx7TKTu8pyloEgW8SViM52q5tjuDh9U=' \
 -H "Content-type: application/json" \
--d '{ test: "ok" }' 'http://localhost:5000/hook'
+-d '{ test: "ok" }' 'http://localhost:5000'
