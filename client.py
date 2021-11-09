399549eb8adfee413822b760050224ad1e673c86
Thomas Daley <tomdaley92@gmail.com>
2021-11-08 23:17:24 -0800

    Add comments to explain hacky parts

--- client.py
+++ client.py
@@ -13,6 +13,8 @@ HEALTHCHECK_INTERVAL = int(os.getenv('HEALTHCHECK_INTERVAL'))
 
 class Client(discord.Client):
     def __init__(self):
+        # idempotent loop reinitialization, since using fork() with asyncio
+        # https://bugs.python.org/issue21998
         asyncio.set_event_loop(asyncio.new_event_loop())
         super().__init__()
         self.loop.create_task(self.healthcheck())
@@ -40,6 +42,10 @@ def is_server_healthy():
 if __name__ == '__main__':
     while True:
         if is_server_healthy():
+            # run disord.py's Client() in a separate process
+            # due to the use of `set_wakeup_fd` which can only
+            # be called from the main thread of the main interpreter
+            # https://docs.python.org/3/library/signal.html#signal.set_wakeup_fd
             if os.fork() == 0: 
                 # child
                 client = Client()
