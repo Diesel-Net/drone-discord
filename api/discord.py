551a0f472b74ea4f06ae123413a916b383418ef1
Thomas Daley <tomdaley92@gmail.com>
2021-11-13 12:49:42 -0800

    Start writing discord util functions

--- api/discord.py
+++ api/discord.py
@@ -1,24 +1,38 @@
-'''
-Discord REST API util functions
-'''
+import os
+import requests
+
+
+DISCORD_TOKEN = os.getenv('DISCORD_TOKEN')
+DISCORD_CHANNEL_ID = os.getenv('DISCORD_CHANNEL_ID')
+
+HEADERS = {
+    'Authorization': f'Bot { DISCORD_TOKEN }'
+}
+
 
 def post_user_created(payload):
     pass
 
+
 def post_user_updated(payload):
     pass
+
 
 def post_user_deleted(payload):
     pass
 
+
 def post_repo_enabled(payload):
     pass
+
 
 def post_repo_disabled(payload):
     pass
 
+
 def post_build_created(payload):
     pass
+
 
 def post_build_updated(payload):
     pass
