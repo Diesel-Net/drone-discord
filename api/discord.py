e268444c3106ccf4f749fffd4b1c46999ed9ca33
Thomas Daley <tomdaley92@gmail.com>
2021-11-13 12:37:34 -0800

    Add discord api function stubs

--- api/discord.py
+++ api/discord.py
@@ -1,3 +1,24 @@
 '''
 Discord REST API util functions
 '''
+
+def post_user_created(payload):
+    pass
+
+def post_user_updated(payload):
+    pass
+
+def post_user_deleted(payload):
+    pass
+
+def post_repo_enabled(payload):
+    pass
+
+def post_repo_disabled(payload):
+    pass
+
+def post_build_created(payload):
+    pass
+
+def post_build_updated(payload):
+    pass
