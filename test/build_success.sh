e70c8b6b254e26e1e872d879c201f0785e27af07
Thomas Daley <tomdaley92@gmail.com>
2021-11-16 08:48:07 -0800

    Add more tests for build success/failures

---
+++ test/build_success.sh
@@ -0,0 +1,9 @@
+# signed with key: webhook-secret
+
+curl -X POST \
+-H 'Signature: keyId="hmac-key",algorithm="hmac-sha256",signature="nMQOWQSyloFPh3ZB8Uoh/D9QyzCtnVf0tk/jemJm058=",headers="date digest"' \
+-H 'X-Drone-Event: build' \
+-H 'Date: Fri, 12 Nov 2021 23:33:47 GMT' \
+-H 'Digest: SHA-256=kCit1zUa+1aDpnZhDSBCa0iVYPTeF6k2jCjHwE2lnQY=' \
+-H "Content-type: application/json" \
+-d @test/build_success.json 'http://localhost:5000'
